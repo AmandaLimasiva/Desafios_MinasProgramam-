a360a8015cd6ce04d9625a17bcbdb000e3db26e2
Amanda Lima <51892828+AmandaLimasiva@users.noreply.github.com>
2022-06-13 21:43:07 -0300

    Desafio Par ou Ímpar

---
+++ script.js
@@ -0,0 +1,7 @@
+let numero = parseFloat(prompt("Informe um numero :)"))
+
+if(numero % 2 === 0){ //Resto da divisão
+    console.log(`O ${numero} informado é PAR`)
+}else{
+    console.log(`O ${numero} informado é íMPAR`)
+}
